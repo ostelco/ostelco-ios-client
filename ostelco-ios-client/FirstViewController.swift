3ad336bd72e09a161bb345e2537bc3dda6d74e99
havard <havardnoren@gmail.com>
2018-10-18 22:29:45 +0200

    Initial Commit

---
+++ ostelco-ios-client/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ostelco-ios-client
+//
+//  Created by mac on 10/18/18.
+//  Copyright © 2018 mac. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
