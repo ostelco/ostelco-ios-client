f02b12e3036da120390c6505ef9217b88415a219
havard <havardnoren@gmail.com>
2018-11-01 13:17:02 +0100

    Notify user if client can't submit payment request

--- ostelco-ios-client/Payment.swift
+++ ostelco-ios-client/Payment.swift
@@ -37,6 +37,20 @@ extension HomeViewController {
         else {
             // There is a problem with your Apple Pay configuration
             os_log("There is a problem with your Apple Pay configuration")
+            // TODO: Report error to bug reporting system
+            #if DEBUG
+                #if targetEnvironment(simulator)
+                    self.showAlert(title: "There is a problem with your Apple Pay configuration", msg: "Apple pay in test mode on simulator is supposed to work. Don't know why it failed.")
+                #else
+                    self.showAlert(title: "There is a problem with your Apple Pay configuration", msg: "Apple pay in test mode on real devices has not been tested yet.")
+                #endif
+            #else
+                #if targetEnvironment(simulator)
+                    self.showAlert(title: "There is a problem with your Apple Pay configuration", msg: "Apple pay in production mode on simulator does not work.")
+                #else
+                    self.showAlert(title: "There is a problem with your Apple Pay configuration", msg: "Apple pay in production mode on real devices has not been tested yet.")
+                #endif
+            #endif
         }
     }
     
