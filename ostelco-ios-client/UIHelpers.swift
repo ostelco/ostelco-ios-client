3b892276b5afda2ea4cdd9e6a3f117282eca12b3
havard <havardnoren@gmail.com>
2018-11-06 18:44:32 +0100

    - Add theming based on debug and release build - hide balance and top up button until we have data - go back to previous VC after auth check instead of always home screen - show launch screen on app startup and resume - move payment extension into HomeVC because compiler complained

--- ostelco-ios-client/UIHelpers.swift
+++ ostelco-ios-client/UIHelpers.swift
@@ -52,3 +52,24 @@ extension UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
 }
+
+class TopUpButton: UIButton {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.layer.cornerRadius = 22.5
+        self.addShadow(offset: CGSize(width: 0.0, height: 15.0), color: UIColor(red: 0, green: 68.0/255.0, blue: 166.0/255.0, alpha: 0.35), radius: 18.0, opacity: 1)
+        self.backgroundColor = ThemeManager.currentTheme().mainColor
+    }
+}
+
+class LaunchScreenViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        #if DEBUG
+            self.imageView.image = UIImage(named: "StoryboardLaunchScreenDevelopment")!
+        #else
+            self.imageView.image = UIImage(named: "StoryboardLaunchScreenProduction")!
+        #endif
+    }
+}
