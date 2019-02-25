cf754e9f55f7a7e0ed19ea0e3bd15d3fb4665edb
havard <havardnoren@gmail.com>
2019-02-25 18:40:29 +0100

    feat: trigger next view controller on unwind for each onboarding step

--- ostelco-ios-client/src/MainController.swift
+++ ostelco-ios-client/src/MainController.swift
@@ -11,56 +11,79 @@ import UIKit
 class MainController: UIViewController {
     
     @IBAction func unwindFromLoginViewController(sender: UIStoryboardSegue) {
-
+        perform(#selector(showSignUp), with: nil, afterDelay: 0)
     }
     
     @IBAction func unwindFromSignUpViewController(sender: UIStoryboardSegue) {
-        
+        perform(#selector(showCountry), with: nil, afterDelay: 0)
     }
     
     @IBAction func unwindFromCountryViewController(sender: UIStoryboardSegue) {
-        
+        perform(#selector(showEKYC), with: nil, afterDelay: 0)
     }
     
     @IBAction func unwindFromEKYCViewController(sender: UIStoryboardSegue) {
-        
+        perform(#selector(showESim), with: nil, afterDelay: 0)
     }
     
     @IBAction func unwindFromESimViewController(sender: UIStoryboardSegue) {
-        
+        perform(#selector(showHome), with: nil, afterDelay: 0)
     }
     
     @IBAction func unwindFromHomeViewController(sender: UIStoryboardSegue) {
         
     }
     
-    @IBAction func showLoginViewController(_ sender: Any) {
-        print("Ssow Login View Controller")
+    @IBAction func showLoginTapped(_ sender: Any?) {
+        self.showLogin()
+    }
+    
+    @IBAction func showSignUpTapped(_ sender: Any) {
+        self.showSignUp()
+    }
+    
+    @IBAction func showCountryTapped(_ sender: Any) {
+        self.showCountry()
+    }
+    
+    @IBAction func showEKYCTapped(_ sender: Any) {
+        self.showEKYC()
+    }
+    
+    @IBAction func showESimTapped(_ sender: Any) {
+        self.showESim()
+    }
+    
+    @IBAction func showHomeTapped(_ sender: Any) {
+        self.showHome()
+    }
+    
+    @objc private func showLogin() {
         let viewController = UIStoryboard(name: "Login", bundle: nil).instantiateInitialViewController() as! LoginViewController2
         present(viewController, animated: true, completion: nil)
     }
     
-    @IBAction func showSignUpViewController(_ sender: Any) {
+    @objc private func showSignUp() {
         let viewController = UIStoryboard(name: "SignUp", bundle: nil).instantiateInitialViewController() as! SignUpViewController
         present(viewController, animated: true, completion: nil)
     }
     
-    @IBAction func showCountryViewController(_ sender: Any) {
+    @objc private func showCountry() {
         let viewController = UIStoryboard(name: "Country", bundle: nil).instantiateInitialViewController() as! CountryViewController
         present(viewController, animated: true, completion: nil)
     }
     
-    @IBAction func showEKYCViewController(_ sender: Any) {
+    @objc private func showEKYC() {
         let viewController = UIStoryboard(name: "EKYC", bundle: nil).instantiateInitialViewController() as! EKYCViewController
         present(viewController, animated: true, completion: nil)
     }
     
-    @IBAction func showESimViewController(_ sender: Any) {
+    @objc private func showESim() {
         let viewController = UIStoryboard(name: "ESim", bundle: nil).instantiateInitialViewController() as! ESimViewController
         present(viewController, animated: true, completion: nil)
     }
     
-    @IBAction func showHomeViewController(_ sender: Any) {
+    @objc private func showHome() {
         let viewController = UIStoryboard(name: "Home", bundle: nil).instantiateInitialViewController() as! HomeViewController2
         present(viewController, animated: true, completion: nil)
     }
