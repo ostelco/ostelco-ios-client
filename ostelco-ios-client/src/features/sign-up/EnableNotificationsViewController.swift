40f7e4653f5aca78275e89f695e61f9c42a237a0
havard <havardnoren@gmail.com>
2019-02-27 15:46:41 +0100

    fix: wrong constraints on SignUp storyboard

--- ostelco-ios-client/src/features/sign-up/EnableNotificationsViewController.swift
+++ ostelco-ios-client/src/features/sign-up/EnableNotificationsViewController.swift
@@ -39,20 +39,39 @@ class EnableNotificationsViewController: UIViewController {
         }
     }
     
+    private func showNotificationAlreadySetAlert(status: String) {
+        let alert = UIAlertController(title: "Notification Alert", message: "your notification status is: \(status)", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+            DispatchQueue.main.async {
+                self.showGetStarted()
+            }
+        }))
+        DispatchQueue.main.async {
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
     private func enableNotifications() {
         UNUserNotificationCenter.current().getNotificationSettings { (settings) in
-            if settings.authorizationStatus == .authorized {
-                self.showNotificationAlreadyEnabledAlert()
+            switch (settings.authorizationStatus) {
+
+            case .notDetermined:
+                self.requestNotificationAuthorization()
+            default:
+                self.showNotificationAlreadySetAlert(status: settings.authorizationStatus.description)
             }
-            else {
-                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) {
-                    (granted, error) in
-                    if (granted) {
-                        self.showNotificationAlreadyEnabledAlert()
-                    } else {
-                        self.showNotificationDisabledAlert()
-                    }
+        }
+    }
+        
+    private func requestNotificationAuthorization() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) {
+            (granted, error) in
+            if (granted) {
+                DispatchQueue.main.async {
+                    self.showGetStarted()
                 }
+            } else {
+                self.showNotificationDisabledAlert()
             }
         }
     }
