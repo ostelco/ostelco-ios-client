31e420e42f01e10472a24e428e7145d033be6936
Prasanth Ullattil <prasanth@telenordigital.com>
2019-02-28 11:29:13 +0100

    Add more screens to SingPass flow

--- ostelco-ios-client/src/features/ekyc/SingPassInfoViewController.swift
+++ ostelco-ios-client/src/features/ekyc/SingPassInfoViewController.swift
@@ -10,21 +10,19 @@ import UIKit
 
 class SingPassInfoViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
 
-        // Do any additional setup after loading the view.
-    }
+  // MARK: - Navigation
 
-  /*
-    // MARK: - Navigation
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    // Get the new view controller using segue.destination.
+    // Pass the selected object to the new view controller.
+  }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
   @IBAction func `continue`(_ sender: Any) {
     performSegue(withIdentifier: "unwindFromSingPassInfoViewController", sender: self)
   }
