950dbb9582305e1678c494d86efd6dfd1e2cef54
havard <havardnoren@gmail.com>
2019-03-05 16:15:11 +0100

    show spinner while fetching user location

---
+++ ostelco-ios-client/src/UIViewController+UIActivityIndicator.swift
@@ -0,0 +1,37 @@
+//
+//  UIViewController+UIActivityIndicator.swift
+//  ostelco-ios-client
+//
+//  Created by mac on 3/5/19.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import UIKit
+
+// TODO: Could be a good idea to not have a global variable. Rather return the view from showSpinner, then use that as a parameter to removeSpinner
+// ref: http://brainwashinc.com/2017/07/21/loading-activity-indicator-ios-swift/
+var vSpinner : UIView?
+
+extension UIViewController {
+    func showSpinner(onView : UIView) {
+        let spinnerView = UIView.init(frame: onView.bounds)
+        spinnerView.backgroundColor = UIColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
+        let ai = UIActivityIndicatorView.init(style: .whiteLarge)
+        ai.startAnimating()
+        ai.center = spinnerView.center
+        
+        DispatchQueue.main.async {
+            spinnerView.addSubview(ai)
+            onView.addSubview(spinnerView)
+        }
+        
+        vSpinner = spinnerView
+    }
+    
+    func removeSpinner() {
+        DispatchQueue.main.async {
+            vSpinner?.removeFromSuperview()
+            vSpinner = nil
+        }
+    }
+}
