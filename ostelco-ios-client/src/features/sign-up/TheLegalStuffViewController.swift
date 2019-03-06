a0c27daa90b5e8b03a752e7a8a6ee813da5f2dc3
havard <havardnoren@gmail.com>
2019-03-06 18:15:03 +0100

    feat(sign-up): style text with links

--- ostelco-ios-client/src/features/sign-up/TheLegalStuffViewController.swift
+++ ostelco-ios-client/src/features/sign-up/TheLegalStuffViewController.swift
@@ -10,4 +10,32 @@ import UIKit
 
 class TheLegalStuffViewController: UIViewController {
     
+    @IBOutlet weak var termsAndConditionsLabel: UILabel!
+    @IBOutlet weak var privacyPolicyLabel: UILabel!
+    @IBOutlet weak var oyaUpdatesLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let attributedString = NSMutableAttributedString(string: "I hereby agree to the Terms & Conditions", attributes: [
+            .font: UIFont.systemFont(ofSize: 16.0, weight: .regular),
+            .foregroundColor: UIColor(white: 50.0 / 255.0, alpha: 1.0)
+            ])
+        attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 16.0, weight: .bold), range: NSRange(location: 22, length: 18))
+        termsAndConditionsLabel.attributedText = attributedString
+        
+        let attributedString2 = NSMutableAttributedString(string: "I agree to the  Privacy Policy", attributes: [
+            .font: UIFont.systemFont(ofSize: 16.0, weight: .regular),
+            .foregroundColor: UIColor(white: 50.0 / 255.0, alpha: 1.0)
+            ])
+        attributedString2.addAttribute(.font, value: UIFont.systemFont(ofSize: 16.0, weight: .bold), range: NSRange(location: 16, length: 14))
+        privacyPolicyLabel.attributedText = attributedString2
+        
+        let attributedString3 = NSMutableAttributedString(string: "I agree to recieve OYA updates by email. This consent can be revoked at any time.", attributes: [
+            .font: UIFont.systemFont(ofSize: 16, weight: .regular),
+            .foregroundColor: UIColor(white: 50.0 / 255.0, alpha: 1.0)
+            ])
+        oyaUpdatesLabel.attributedText = attributedString3
+        
+    }
 }
