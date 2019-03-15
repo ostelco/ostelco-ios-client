ef07cf955da474b4e10ee3720a4b4f0dcff7514a
havard <havardnoren@gmail.com>
2019-03-15 11:20:53 +0100

    feat(home): add pull to refresh and move label from bottom to top

--- ostelco-ios-client/src/features/home/HomeViewController2.swift
+++ ostelco-ios-client/src/features/home/HomeViewController2.swift
@@ -11,8 +11,23 @@ import UIKit
 class HomeViewController2: UIViewController {
     
     @IBOutlet weak var balanceLabel: UILabel!
-    
+    @IBOutlet weak var scrollView: UIScrollView!
+    var refreshControl: UIRefreshControl!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scrollView.alwaysBounceVertical = true
+        scrollView.bounces  = true
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(didPullToRefresh), for: .valueChanged)
+        self.scrollView.addSubview(refreshControl)
+    }
+    
+    @objc func didPullToRefresh() {
+        let delayInSeconds = 4.0
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + delayInSeconds) {
+            self.refreshControl?.endRefreshing()
+        }
     }
 }
