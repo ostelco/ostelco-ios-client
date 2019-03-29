b93b8b9cb2f2abe7d4958e1d0c1ac5f6b9f5cfb2
Prasanth Ullattil <prasanth@telenordigital.com>
2019-03-29 13:53:22 +0100

    Pass authentication code from MyInfo to prime

--- ostelco-ios-client/src/features/ekyc/MyInfoSummaryViewController.swift
+++ ostelco-ios-client/src/features/ekyc/MyInfoSummaryViewController.swift
@@ -9,22 +9,52 @@
 import UIKit
 
 class MyInfoSummaryViewController: UIViewController {
+    public var myInfoQueryItems: [URLQueryItem]?
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view.
-  }
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-  // MARK: - Navigation
+        print("Query Items", myInfoQueryItems)
+        if let code = getMyInfoCode() {
+            print("Code = \(code)")
+            APIManager.sharedInstance.regions.child("/sg/kyc/myInfo").child(code).load()
+                .onSuccess { entity in
+                    print("------------_")
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: entity.content as! Data, options: []) as? [String : Any]
+                        print(json)
+                    } catch {
 
-  // In a storyboard-based application, you will often want to do a little preparation before navigation
-  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    // Get the new view controller using segue.destination.
-    // Pass the selected object to the new view controller.
-  }
+                    }
+                    print("------------_")
+                }
+                .onFailure { error in
+                    self.showAPIError(error: error)
+            }
+        }
+        //TODO: Pass the code we retrieved to PRIME
+        //TODO: Get the address & phone number form PRIME
 
-  @IBAction func `continue`(_ sender: Any) {
-    performSegue(withIdentifier: "ESim", sender: self)
-  }
+    }
+
+    private func getMyInfoCode() -> String? {
+        if let queryItems = myInfoQueryItems {
+            if let codeItem = queryItems.first(where: { $0.name == "code" }) {
+                return codeItem.value
+            }
+        }
+        return nil
+    }
+
+    // MARK: - Navigation
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+    @IBAction func `continue`(_ sender: Any) {
+        performSegue(withIdentifier: "ESim", sender: self)
+    }
 
 }
--- ostelco-ios-client/src/features/ekyc/SelectIdentityVerificationMethodViewController.swift
+++ ostelco-ios-client/src/features/ekyc/SelectIdentityVerificationMethodViewController.swift
@@ -10,61 +10,68 @@ import UIKit
 import SafariServices
 
 class SelectIdentityVerificationMethodViewController: UIViewController {
-  var webView: SFSafariViewController?
-  @IBAction func singPassTapped(_ sender: Any) {
-    //performSegue(withIdentifier: "myInfoSummary", sender: self)
-    let appDelegate = UIApplication.shared.delegate as! AppDelegate
-    appDelegate.myInfoDelegate = self
-    getMyInfoToken()
-  }
+    var webView: SFSafariViewController?
+    var myInfoQueryItems: [URLQueryItem]?
 
-  @IBAction func nricTapped(_ sender: Any) {
-    performSegue(withIdentifier: "nricVerify", sender: self)
-  }
-
-  @IBAction func needHelpTapped(_ sender: Any) {
-    showNeedHelpActionSheet()
-  }
-
-  func getMyInfoURL() -> URL? {
-    var components = URLComponents(string: Environment().configuration(PlistKey.MyInfoURL))!
-    components.queryItems = [
-      URLQueryItem(name: "client_id", value: Environment().configuration(PlistKey.MyInfoClientID)),
-       //TODO: Find the right values for the query parameters.
-      URLQueryItem(name: "attributes", value: "name,nationality,dob,email,mobileno,regadd"),
-      URLQueryItem(name: "purpose", value: "eKYC"),
-      URLQueryItem(name: "state", value: "123"),
-      URLQueryItem(name: "redirect_uri", value: Environment().configuration(PlistKey.MyInfoCallbackURL)),
-    ]
-    return components.url
-  }
-
-  func getMyInfoToken() {
-    if let url = getMyInfoURL() {
-      print("URL for API \(url.absoluteString)")
-      webView = SFSafariViewController(url: url)
-      webView!.delegate = self
-      present(webView!, animated: true)
+    @IBAction func singPassTapped(_ sender: Any) {
+        //performSegue(withIdentifier: "myInfoSummary", sender: self)
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.myInfoDelegate = self
+        getMyInfoToken()
     }
-  }
+
+    @IBAction func nricTapped(_ sender: Any) {
+        performSegue(withIdentifier: "nricVerify", sender: self)
+    }
+
+    @IBAction func needHelpTapped(_ sender: Any) {
+        showNeedHelpActionSheet()
+    }
+
+    func getMyInfoURL() -> URL? {
+        var components = URLComponents(string: Environment().configuration(PlistKey.MyInfoURL))!
+        components.queryItems = [
+            URLQueryItem(name: "client_id", value: Environment().configuration(PlistKey.MyInfoClientID)),
+            //TODO: Find the right values for the query parameters.
+            URLQueryItem(name: "attributes", value: "name,nationality,dob,email,mobileno,regadd"),
+            URLQueryItem(name: "purpose", value: "eKYC"),
+            URLQueryItem(name: "state", value: "123"),
+            URLQueryItem(name: "redirect_uri", value: Environment().configuration(PlistKey.MyInfoCallbackURL)),
+        ]
+        return components.url
+    }
+
+    func getMyInfoToken() {
+        if let url = getMyInfoURL() {
+            print("URL for API \(url.absoluteString)")
+            webView = SFSafariViewController(url: url)
+            webView!.delegate = self
+            present(webView!, animated: true)
+        }
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let id = segue.identifier, id == "myInfoSummary",
+            let destinationVC = segue.destination as? MyInfoSummaryViewController {
+            destinationVC.myInfoQueryItems = myInfoQueryItems
+        }
+    }
 }
 
 extension SelectIdentityVerificationMethodViewController: SFSafariViewControllerDelegate {
-  func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
-    dismiss(animated: true)
-    webView = nil
-  }
+    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+        dismiss(animated: true)
+        webView = nil
+    }
 }
 
 extension SelectIdentityVerificationMethodViewController: MyInfoCallbackHandler {
-  func handleCallback(queryItems: [URLQueryItem]?, error: NSError?) {
-    print("Query Items", queryItems)
-    //TODO: Pass the code we retrieved to PRIME
-    //TODO: Get the address & phone number form PRIME
-    dismiss(animated: false) { [weak self] in
-      // Show the information gathered from MyInfo
-      self?.performSegue(withIdentifier: "myInfoSummary", sender: self)
+    func handleCallback(queryItems: [URLQueryItem]?, error: NSError?) {
+        dismiss(animated: false) { [weak self] in
+            // Show the information gathered from MyInfo
+            self?.myInfoQueryItems = queryItems
+            self?.performSegue(withIdentifier: "myInfoSummary", sender: self)
+        }
+        webView = nil
     }
-    webView = nil
-  }
 }
