37bba60c40aa9226e2b82f41e4060644d83f32c3
havard <havardnoren@gmail.com>
2019-04-03 13:45:31 +0200

    add code to generate sim profile if none exists and handle errors by redirecting to generic error screen

--- ostelco-ios-client/src/features/esim/ESIMPendingDownloadViewController.swift
+++ ostelco-ios-client/src/features/esim/ESIMPendingDownloadViewController.swift
@@ -17,4 +17,57 @@ class ESIMPendingDownloadViewController: UIViewController {
     @IBAction func needHelpTapped(_ sender: Any) {
         showNeedHelpActionSheet()
     }
+    
+    override func viewDidLoad() {
+        let region  = OnBoardingManager.sharedInstance.region!
+        let countryCode = region.region.id
+        
+        if let simProfiles = region.simProfiles, simProfiles.count > 0 {
+            let simProfile = simProfiles[0]
+        } else {
+            showSpinner(onView: self.view)
+            APIManager.sharedInstance.regions.child(countryCode).child("simProfiles").request(.post)
+                .onSuccess { data in
+                    if let simProfile: SimProfile = data.typedContent(ifNone: nil) {
+                        
+                    } else {
+                        DispatchQueue.main.async {
+                            self.performSegue(withIdentifier: "showGenericOhNo", sender: self)
+                        }
+                    }
+                }
+                .onFailure { requestError in
+                    if let statusCode = requestError.httpStatusCode {
+                        switch statusCode {
+                        default:
+                            DispatchQueue.main.async {
+                                self.performSegue(withIdentifier: "showGenericOhNo", sender: self)
+                            }
+                        }
+                    } else {
+                        DispatchQueue.main.async {
+                            self.performSegue(withIdentifier: "showGenericOhNo", sender: self)
+                        }
+                    }
+                }
+                .onCompletion { _ in
+                    self.removeSpinner()
+                }
+        }
+        
+        /*
+        showSpinner(onView: self.view)
+        APIManager.sharedInstance.regions.child(countryCode)
+            .load()
+            .onSuccess { response in
+                
+            }
+            .onFailure { requestError in
+                
+            }
+            .onCompletion { _ in
+                self.removeSpinner()
+            }
+         */
+    }
 }
