07437c96fbdf4808eed9623955a0a1af472b2c86
havard <havardnoren@gmail.com>
2019-04-03 18:19:27 +0200

    set user info, id and properties for freshchat

--- ostelco-ios-client/src/services/UserManager.swift
+++ ostelco-ios-client/src/services/UserManager.swift
@@ -12,7 +12,22 @@ class UserManager {
     static let sharedInstance = UserManager()
 
     var authToken: String?
-    var user: CustomerModel!
+    var user: CustomerModel! {
+        didSet {
+            if user == nil {
+                Freshchat.sharedInstance().resetUser(completion: { () in
+                    //Completion code
+                })
+            } else {
+                Freshchat.sharedInstance().identifyUser(withExternalID: user.id, restoreID: nil)
+                let fcUser = FreshchatUser.sharedInstance();
+                fcUser?.firstName = user.name
+                fcUser?.email = user.email
+                Freshchat.sharedInstance().setUser(fcUser)
+            }
+            
+        }
+    }
 
     func clear() {
         authToken = nil
