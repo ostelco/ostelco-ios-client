a882b30a2235397fe6b66fbc48fa6ae0e21842ec
Prasanth Ullattil <prasanth@telenordigital.com>
2019-04-04 15:19:18 +0200

    Update log line

--- ostelco-ios-client/src/services/Auth.swift
+++ ostelco-ios-client/src/services/Auth.swift
@@ -24,7 +24,7 @@ class Auth {
         Auth0
             .webAuth()
             .clearSession(federated: true) {
-                print($0)
+                print("Clear Session: ", $0)
                 _ = self.credentialsManager.clear()
         }
     }
@@ -39,18 +39,17 @@ class Auth {
     func loginWithAuth0() -> Observable<Credentials> {
         os_log("Start login with auth0...")
         var params: [String:String] = [:]
-        
+
         if forceLoginPrompt {
             params["prompt"] = "login"
         }
-        
+
         return Observable.create { observer in
             Auth0
                 .webAuth()
                 .logging(enabled: true)
                 .audience("http://google_api")
                 .scope("openid email profile offline_access")
-                // .connection("google-oauth2")
                 .parameters(params)
                 .start {
                     switch $0 {
@@ -69,10 +68,10 @@ class Auth {
                             os_log("Failed to login with auth0, got non web auth error: %{public}@", "\(error)")
                             observer.on(.error(error))
                         }
-                        
+
                     case .success(let credentials):
                         os_log("Store credentials with auth0 credentials manager.")
-                        self.credentialsManager.store(credentials: credentials)
+                        _ = self.credentialsManager.store(credentials: credentials)
                         os_log("Successfully logged in with auth0, credential. refreshToken: %{private}@ accessToken: %{private}@ idToken: %{private}@", credentials.refreshToken ?? "none", credentials.accessToken ?? "none", credentials.idToken ?? "none")
                         if let accessToken = credentials.accessToken {
                             DispatchQueue.main.async {
@@ -82,7 +81,7 @@ class Auth {
                         } else {
                             // TODO: How do we handle the case if access token is empty
                         }
-                        
+
                         self.forceLoginPrompt = false
                         observer.on(.next(credentials))
                         observer.on(.completed)
