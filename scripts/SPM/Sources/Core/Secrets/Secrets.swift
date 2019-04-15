7a91d255620a43c652cab40083bd8920f317ab87
Ellen Shapiro <designatednerd@gmail.com>
2019-04-15 11:29:35 +0200

    inline documentation for all public methods in `SPM`

--- scripts/SPM/Sources/Core/Secrets/Secrets.swift
+++ scripts/SPM/Sources/Core/Secrets/Secrets.swift
@@ -8,11 +8,14 @@
 import Foundation
 import Files
 
+/// A struct to contain grabbing and/or resetting all secrets.
 struct Secrets {
     
+    /// Errors which can occur when generating secrets.
+    ///
+    /// - missingSecrets: Some secrets were missing from either `secrets.json` or the environment.
+    ///                   Parameter is an array of the missing secret keys.
     enum Error: Swift.Error {
-        case couldNotAccessGitRoot
-        case noSecretsFileOrSecrets(errorMessage: String)
         case missingSecrets(keyNames: [String])
     }
     
@@ -26,6 +29,11 @@ struct Secrets {
         }
     }
     
+    /// Takes secrets from either JSON or the environment and puts them into the appropriate plist files.
+    ///
+    /// - Parameters:
+    ///   - sourceRoot: The main iOS project's source root
+    ///   - forProd: True if the production values should be used, false if the dev values should be used.
     static func run(sourceRoot: Folder, forProd: Bool) throws {
         let secrets: [String: AnyHashable]
         if self.localJSONExists(sourceRoot: sourceRoot, forProd: forProd) {
@@ -41,6 +49,9 @@ struct Secrets {
         try EnvironmentUpdater.run(secrets: secrets, sourceRoot: sourceRoot)
     }
     
+    /// Resets any plist files which were changed.
+    ///
+    /// - Parameter sourceRoot: The main iOS project's source root
     static func reset(sourceRoot: Folder) throws {
         try FirebaseUpdater.reset(sourceRoot: sourceRoot)
         try Auth0Updater.reset(sourceRoot: sourceRoot)
