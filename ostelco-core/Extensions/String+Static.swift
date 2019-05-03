65e37e8209b914df12fd56057080408d18efa9f6
Ellen Shapiro <designatednerd@gmail.com>
2019-05-03 14:40:37 +0200

    add some docs to the static string initializer

--- ostelco-core/Extensions/String+Static.swift
+++ ostelco-core/Extensions/String+Static.swift
@@ -10,6 +10,9 @@ import Foundation
 
 public extension String {
     
+    /// Initializer which is inexplicably not in the stdlib
+    ///
+    /// - Parameter staticString: The static string to use to create a string
     init(staticString: StaticString) {
         self = staticString.withUTF8Buffer {
             String(decoding: $0, as: UTF8.self)
