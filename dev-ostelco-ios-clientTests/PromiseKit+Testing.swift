97c57f4cf4eb6f399b196203893dc2ca6475ef88
Ellen Shapiro <designatednerd@gmail.com>
2019-05-09 18:10:39 +0200

    Add testing extension to promise to allow awaiting without blocking the main thread

---
+++ dev-ostelco-ios-clientTests/PromiseKit+Testing.swift
@@ -0,0 +1,37 @@
+//
+//  PromiseKit+Testing.swift
+//  dev-ostelco-ios-clientTests
+//
+//  Created by Ellen Shapiro on 5/9/19.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import PromiseKit
+import XCTest
+
+extension Promise {
+    
+    func awaitResult(in testCase: XCTestCase,
+                     timeout: TimeInterval = 10,
+                     file: StaticString = #file,
+                     line: UInt = #line) -> T? {
+        let expectation = testCase.expectation(description: "Awaiting fulfillment of promise")
+        
+        var result: T?
+        self
+            .done { item in
+                result = item
+                expectation.fulfill()
+            }
+            .catch { error in
+                XCTFail("Error awaiting result: \(error)",
+                        file: file,
+                        line: line)
+                
+                expectation.fulfill()
+            }
+        
+        testCase.wait(for: [expectation], timeout: timeout)
+        return result
+    }
+}
