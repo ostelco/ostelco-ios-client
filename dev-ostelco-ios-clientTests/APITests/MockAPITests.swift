0d55062a8fbb76969b183a1e6cf730cf8a6b7a5b
Ellen Shapiro <designatednerd@gmail.com>
2019-05-09 18:11:36 +0200

    Add initial mock API tests and mock data

---
+++ dev-ostelco-ios-clientTests/APITests/MockAPITests.swift
@@ -0,0 +1,98 @@
+//
+//  MockAPITests.swift
+//  dev-ostelco-ios-clientTests
+//
+//  Created by Ellen Shapiro on 5/9/19.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import OHHTTPStubs
+import ostelco_core
+import PromiseKit
+import XCTest
+
+class MockAPITests: XCTestCase {
+    
+    private lazy var testAPI = PrimeAPI(baseURL: "https://www.fake.org/api", tokenProvider: self.mockTokenProvider)
+    
+    private lazy var mockTokenProvider: MockTokenProvider = {
+        let provider = MockTokenProvider()
+        provider.initialToken = "Initial Test Token"
+        return provider
+    }()
+    
+    // MARK: - Test Lifecycle
+    
+    override func setUp() {
+        super.setUp()
+        OHHTTPStubs.setEnabled(true)
+    }
+    
+    override func tearDown() {
+        OHHTTPStubs.removeAllStubs()
+        OHHTTPStubs.setEnabled(false)
+        super.tearDown()
+    }
+    
+    // MARK: - Test Helpers
+    
+    private func stubPath(_ path: String,
+                          toLoad fileName: String,
+                          statusCode: Int32 = 200,
+                          file: StaticString = #file,
+                          line: UInt = #line) {
+        OHHTTPStubs.stubRequests(passingTest: isPath("/api/\(path)"), withStubResponse: { _ in
+            guard let path = Bundle(for: MockAPITests.self).path(forResource: fileName, ofType: "json", inDirectory: "MockJSON") else {
+                XCTFail("Couldn't get bundled path!",
+                        file: file,
+                        line: line)
+                return OHHTTPStubsResponse(error: NSError(domain: "", code: 0, userInfo: nil))
+            }
+            
+            return OHHTTPStubsResponse(fileAtPath: path, statusCode: statusCode, headers: nil)
+        })
+    }
+    
+    func testMockFetchingContext() throws {
+        self.stubPath("context", toLoad: "context")
+        
+        guard let context = self.testAPI.loadContext().awaitResult(in: self) else {
+            // Failure handled in `awaitResult`
+            return
+        }
+        
+        guard let customer = context.customer else {
+            XCTFail("No customer in context!")
+            return
+        }
+        
+        XCTAssertEqual(customer.id, "e30665f1-2a08-4304-bc06-5005b268b3b8")
+        XCTAssertEqual(customer.analyticsId, "7966e40e-e85a-46fd-953d-14e86bb0afec")
+        XCTAssertEqual(customer.referralId, "f3562c3a-8a6e-4be1-a521-7a2c7b1c2b41")
+        XCTAssertEqual(customer.email, "steve@apple.com")
+        XCTAssertEqual(customer.name, "Steve")
+        XCTAssertFalse(customer.hasSubscription())
+        
+        XCTAssertEqual(context.regions.count, 1)
+
+        guard let firstRegion = context.regions.first else {
+            XCTFail("Context regions was empty!")
+            return
+        }
+        
+        XCTAssertEqual(firstRegion.region.id, "sg")
+        XCTAssertEqual(firstRegion.region.name, "Singapore")
+        XCTAssertEqual(firstRegion.status, .PENDING)
+        XCTAssertEqual(firstRegion.kycStatusMap.JUMIO, .REJECTED)
+        XCTAssertEqual(firstRegion.kycStatusMap.MY_INFO, .PENDING)
+        XCTAssertEqual(firstRegion.kycStatusMap.ADDRESS_AND_PHONE_NUMBER, .PENDING)
+        XCTAssertEqual(firstRegion.kycStatusMap.NRIC_FIN, .APPROVED)
+    
+        guard let simProfiles = firstRegion.simProfiles else {
+            XCTFail("Sim profiles was null instead of empty!")
+            return
+        }
+        
+        XCTAssertTrue(simProfiles.isEmpty)
+    }
+}
