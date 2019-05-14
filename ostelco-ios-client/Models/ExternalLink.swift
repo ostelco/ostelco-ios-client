5a7167a71036f1e7ab50747a3b454dd7f164a1c1
Ellen Shapiro <designatednerd@gmail.com>
2019-05-14 19:01:34 +0200

    test external links work

--- ostelco-ios-client/Models/ExternalLink.swift
+++ ostelco-ios-client/Models/ExternalLink.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2019 mac. All rights reserved.
 //
 
-enum ExternalLink: String {
+enum ExternalLink: String, CaseIterable {
     case privacyPolicy = "https://pi-redirector.firebaseapp.com/privacy-policy"
     case termsAndConditions = "https://pi-redirector.firebaseapp.com/terms-and-conditions"
     
