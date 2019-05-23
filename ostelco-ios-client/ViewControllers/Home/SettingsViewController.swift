7e9e62a142df692612eba75e9a7581c14d85d50c
Prasanth Ullattil <prasanth@telenordigital.com>
2019-05-23 10:17:12 +0200

    Add application version and build number

---
+++ ostelco-ios-client/ViewControllers/Home/SettingsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SettingsViewController.swift
+//  ostelco-ios-client
+//
+//  Created by Prasanth Ullattil on 23/05/2019.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet private weak var versionLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var appVersion = ""
+        if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+            appVersion = "App Version \(version)"
+        }
+        if let buildNumber = Bundle.main.infoDictionary?["CFBundleVersion"] as? String {
+            appVersion = "\(appVersion) Build \(buildNumber)"
+        }
+        self.versionLabel.text = appVersion
+    }
+}
