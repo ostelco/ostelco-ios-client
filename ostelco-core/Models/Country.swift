e5b394f06c61ac6a995dc4e3bfa64ed4ff4799e9
Ellen Shapiro <designatednerd@gmail.com>
2019-06-04 17:45:31 +0200

    Add convenience method to get three-letter country code for countries we're supporting

--- ostelco-core/Models/Country.swift
+++ ostelco-core/Models/Country.swift
@@ -29,6 +29,21 @@ public class Country: Equatable {
         self.countryCode = countryCode
     }
 
+    public var threeLetterCountryCode: String? {
+        switch self.countryCode.lowercased() {
+        case "sg":
+            return "SGP"
+        case "no":
+            return "NOR"
+        case "us":
+            return "USA"
+        default:
+            // TODO: Get a full mapping of 2 digit to 3 digit codes for Jumio
+            // https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3
+            return nil
+        }
+    }
+
     // MARK: - Equatable
     
     public static func == (lhs: Country, rhs: Country) -> Bool {
