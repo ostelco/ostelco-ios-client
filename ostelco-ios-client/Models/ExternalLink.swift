9c2a43d36f5d5c63538e12146e1e2085591f5e2e
Ellen Shapiro <designatednerd@gmail.com>
2019-06-12 22:33:36 +0200

    Remove warning now that links are set up with redirector

--- ostelco-ios-client/Models/ExternalLink.swift
+++ ostelco-ios-client/Models/ExternalLink.swift
@@ -10,8 +10,6 @@ enum ExternalLink: String, CaseIterable {
     case privacyPolicy = "https://pi-redirector.firebaseapp.com/privacy-policy"
     case termsAndConditions = "https://pi-redirector.firebaseapp.com/terms-and-conditions"
     case esimInstructionsVideo = "https://pi-redirector.firebaseapp.com/esim-instructions-video"
-    
-    #warning("Get real link for the below links before shipping!")
     case minimumAgeDetails = "https://pi-redirector.firebaseapp.com/minimum-age"
     case locationRequirement = "https://pi-redirector.firebaseapp.com/location-required"
     case aboutMembership = "https://pi-redirector.firebaseapp.com/about-membership"
