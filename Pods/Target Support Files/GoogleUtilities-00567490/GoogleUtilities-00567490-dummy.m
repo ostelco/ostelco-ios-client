8a12e8609d6677849c00af82ca2a5d648b10ea93
Prasanth Ullattil <prasanth@telenordigital.com>
2019-06-14 14:24:29 +0200

    Remove Firebase/Performance
    Archive is failing when we add this. Adding this to podfile will
    add 2 GooleUtilities target in Pod Project file

--- Pods/Target Support Files/GoogleUtilities-00567490/GoogleUtilities-00567490-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_GoogleUtilities_00567490 : NSObject
-@end
-@implementation PodsDummy_GoogleUtilities_00567490
-@end
--- Pods/Target Support Files/GoogleUtilities-ac14cd06/GoogleUtilities-ac14cd06-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_GoogleUtilities_ac14cd06 : NSObject
-@end
-@implementation PodsDummy_GoogleUtilities_ac14cd06
-@end
