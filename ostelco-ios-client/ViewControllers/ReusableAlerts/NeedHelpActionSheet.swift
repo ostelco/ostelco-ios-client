fa620bbb4ae0f4d3c9b5fab7073cabbe9ccd5085
Håvard Norén <havardnoren@gmail.com>
2019-06-18 15:01:23 +0200

    show freshchat FAQ instead of chat when clicking "Contact support"

--- ostelco-ios-client/ViewControllers/ReusableAlerts/NeedHelpActionSheet.swift
+++ ostelco-ios-client/ViewControllers/ReusableAlerts/NeedHelpActionSheet.swift
@@ -14,7 +14,16 @@ class NeedHelpAlertController: UIAlertController {
         self.init(title: nil, message: nil, preferredStyle: .actionSheet)
         
         let supportAction = UIAlertAction(title: "Contact Support", style: .default) {_ in
-            Freshchat.sharedInstance()?.showConversations(viewController)
+            
+            let options = FAQOptions.init()
+            
+            /*
+            ref: https://support.freshchat.com/support/solutions/articles/229394-freshchat-ios-sdk-swift-integration
+            options.showFaqCategoriesAsGrid = false; // Set false to show faq in list view
+            options.showContactUsOnAppBar = true; // Set false to hide contact us from app bar
+            options.showContactUsOnFaqScreens = true; // Set false to hide contact us bottom banner view
+            */
+            Freshchat.sharedInstance()?.showFAQs(viewController, with: options)
         }
         self.addAction(supportAction)
         
