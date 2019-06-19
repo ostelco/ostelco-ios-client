777a84fa1323846d0bfc2930a81fb002334a5f73
Håvard Norén <havardnoren@gmail.com>
2019-06-19 15:32:05 +0200

    remove pod firebase/core and add pod firebase/analytics

--- Pods/Target Support Files/Pods-ostelco-ios-ostelco-ios-client/Pods-ostelco-ios-ostelco-ios-client-frameworks.sh
+++ Pods/Target Support Files/Pods-ostelco-ios-ostelco-ios-client/Pods-ostelco-ios-ostelco-ios-client-frameworks.sh
@@ -153,8 +153,7 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities-00567490/GoogleUtilities.framework"
   install_framework "${PODS_ROOT}/JumioMobileSDK/JumioMobileSDK-3.1.2/JumioCore.framework"
   install_framework "${PODS_ROOT}/JumioMobileSDK/JumioMobileSDK-3.1.2/Netverify.framework"
   install_framework "${PODS_ROOT}/JumioMobileSDK/JumioMobileSDK-3.1.2/NetverifyBarcode.framework"
@@ -163,12 +162,12 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${PODS_ROOT}/JumioMobileSDK/JumioMobileSDK-3.1.2/ZoomAuthenticationHybrid.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Protobuf/Protobuf.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Stripe/Stripe.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GTMSessionFetcher/GTMSessionFetcher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit/PromiseKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities-00567490/GoogleUtilities.framework"
   install_framework "${PODS_ROOT}/JumioMobileSDK/JumioMobileSDK-3.1.2/JumioCore.framework"
   install_framework "${PODS_ROOT}/JumioMobileSDK/JumioMobileSDK-3.1.2/Netverify.framework"
   install_framework "${PODS_ROOT}/JumioMobileSDK/JumioMobileSDK-3.1.2/NetverifyBarcode.framework"
@@ -177,6 +176,7 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${PODS_ROOT}/JumioMobileSDK/JumioMobileSDK-3.1.2/ZoomAuthenticationHybrid.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Protobuf/Protobuf.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Stripe/Stripe.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GTMSessionFetcher/GTMSessionFetcher.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit/PromiseKit.framework"
 fi
