5774eb9c1c3fa358c786cf863c0222aba0033664
Samuel Goodwin <samuel@roundwallsoftware.com>
2019-07-17 08:54:52 +0200

    Add in some tests for new OnboardingCoordinator
    All the business logic is managed by the StageDecider, the coordinator
    just needs to rely on the StageDecider and update contexts accordingly.
    We can test to make sure buggy scenarios don't come back. I've added
    this intial test as an example.

---
+++ dev-ostelco-ios-clientTests/CoordinatorTests/OnboardingCoordinatorTests.swift
@@ -0,0 +1,71 @@
+//
+//  RootCoordinatorTests.swift
+//  dev-ostelco-ios-clientTests
+//
+//  Created by Ellen Shapiro on 6/5/19.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+@testable import ostelco_core
+@testable import Oya_Development_app
+import XCTest
+import PromiseKit
+
+class OnboardingCoordinatorTests: XCTestCase {
+    
+    enum Errors: Error {
+        case noContextFound
+    }
+    
+    class FakeNavigation: UINavigationController {
+        var trappedControllers = [UIViewController]()
+        
+        override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+            trappedControllers.append(viewController)
+        }
+        
+        override func setViewControllers(_ viewControllers: [UIViewController], animated: Bool) {
+            trappedControllers.append(contentsOf: viewControllers)
+        }
+    }
+    
+    class FakeTokenProvider: TokenProvider {
+        func getToken() -> Promise<String> {
+            return Promise.value("xxxx")
+        }
+        
+        func forceRefreshToken() -> Promise<String> {
+            return Promise.value("xxxx")
+        }
+    }
+    
+    class FakePrimeAPI: PrimeAPI {
+        var fakeContext: Context?
+        
+        convenience init() {
+            
+            self.init(baseURL: "https://google.com", tokenProvider: FakeTokenProvider())
+        }
+        
+        override func loadContext() -> Promise<Context> {
+            if let context = fakeContext {
+                return Promise.value(context)
+            }
+            return Promise(error: Errors.noContextFound)
+        }
+    }
+    
+    func testCoordinatorHandlesUseSeeingLoginCarousel() {
+        let fake = FakeNavigation()
+        let fakePrime = FakePrimeAPI()
+        let testCoordinator = OnboardingCoordinator(navigationController: fake, primeAPI: fakePrime)
+        
+        testCoordinator.loginCarouselSeen()
+        
+        AssertAllAreClass(fake.trappedControllers, aClass: EmailEntryViewController.self)
+    }
+    
+    func AssertAllAreClass(_ items: [NSObject], aClass: AnyClass, file: StaticString = #file, line: UInt = #line) {
+        XCTAssert(items.allSatisfy({ $0.isKind(of: aClass) }), file: file, line: line)
+    }
+}
