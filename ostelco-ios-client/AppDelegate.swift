4b092aeb9a044fd2f58ef48bd1b98f5ab4b78ddf
Håvard Norén <havardnoren@gmail.com>
2019-10-01 14:06:54 +0200

    Add deep link to log user in to firebase using username and password.

--- ostelco-ios-client/AppDelegate.swift
+++ ostelco-ios-client/AppDelegate.swift
@@ -115,6 +115,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         debugPrint("Incoming URL is \(incomingURL)")
         
+        if UsernameAndPasswordLinkManager.isUsernameAndPasswordLink(incomingURL) {
+            debugPrint("Login with username and password...")
+           UsernameAndPasswordLinkManager.signInWithUsernameAndPassword(incomingURL)
+               .done { _ in
+
+               }
+               .catch { error in
+                   ApplicationErrors.log(error)
+                   debugPrint("ERROR SIGNING IN WITH USERNAME AND PASSWORD: \(error)")
+               }
+           
+           return true
+       }
+        
         // If we've gotten here, it's some other kind of universal link.
         return DynamicLinks.dynamicLinks().handleUniversalLink(incomingURL) { dynamicLink, error in
             guard error == nil else {
