3463f13a6c4b69d9aaecde178dea235da05f98a4
Håvard Norén <havardnoren@gmail.com>
2019-10-09 06:56:38 +0200

    Present modal when clicking on a region card
    - Rename RegionView to RegionCardView
    - Move rounded border and shadow style out of RegionCardView so we can use it as is in RegionView
    - Wrap RegionCardView in a button that opens a dummy modal

--- ostelco-ios-client/Views/CoverageView.swift
+++ ostelco-ios-client/Views/CoverageView.swift
@@ -12,62 +12,8 @@ import OstelcoStyles
 import ostelco_core
 
 struct RegionView: View {
-    let label: String
-    let description: String
-    let centerText: String?
-    let backgroundColor: Color
-    
-    init(label: String, description: String, centerText: String? = nil, backgroundColor: Color) {
-        self.label = label
-        self.description = description
-        self.centerText = centerText
-        self.backgroundColor = backgroundColor
-    }
-    
     var body: some View {
-        Group {
-            Image(uiImage: UIImage.ostelco_illustration)
-        }.frame(maxWidth: .infinity)
-        .padding(.top, 70)
-        .background(backgroundColor)
-        .cornerRadius(28)
-        .clipped()
-        .shadow(color: OstelcoColor.regionShadow.toColor, radius: 16, x: 0, y: 6)
-        .overlay(
-            HStack { // Are there any other ways to create a view that fills up its parent?
-                VStack {
-                    Spacer()
-                }
-                Spacer()
-            }
-            .background(centerText != nil ? Color(red: 0, green: 0, blue: 0, opacity: 0.6) : Color(red: 0, green: 0, blue: 0, opacity: 0)) // Is there a way to only render this component if centerText != nil instead of rendering the component with a transparent background
-            .cornerRadius(28)
-        )
-        .overlay(
-            ZStack {
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text(description.uppercased())
-                        .font(.system(size: 13))
-                        .foregroundColor(Color.white)
-                        .frame(height: 27)
-                        .opacity(0.75)
-                        .fixedSize()
-                        Text(label)
-                        .font(.system(size: 28, weight: .bold))
-                        .foregroundColor(Color.white)
-                        .fixedSize()
-                        Spacer()
-                    }
-                    Spacer()
-                }.padding(20)
-                centerText.map {
-                    Text($0)
-                    .font(.system(size: 28, weight: .bold))
-                    .foregroundColor(Color.white)
-                }
-            }
-        )
+        Text("Test")
     }
 }
 
@@ -76,6 +22,7 @@ struct CoverageView: View {
     @ObservedObject var store = AppStore()
     let controller: CoverageViewController
     let countries = ["SE", "HK", "IN", "MY", "NO", "PH", "SG", "TH", "US"].map { Country($0) }
+    @State private var showModal: Bool = false
     
     init(controller: CoverageViewController) {
         self.controller = controller
@@ -106,8 +53,16 @@ struct CoverageView: View {
                     }
                     
                     OstelcoTitle(label: "All Destinations")
-                    RegionView(label: "Asia", description: "Southeast asia & pacific", backgroundColor: OstelcoColor.lipstick.toColor)
-                    RegionView(label: "The Americas", description: "Latin & north america", centerText: "Coming Soon", backgroundColor: OstelcoColor.azul.toColor)
+                    
+                    Button(action: {
+                        debugPrint(self.showModal)
+                        self.showModal = true
+                    }) {
+                        RegionCardView(label: "Asia", description: "Southeast asia & pacific", backgroundColor: OstelcoColor.lipstick.toColor)
+                    }.cornerRadius(28)
+                    .clipped()
+                    .shadow(color: OstelcoColor.regionShadow.toColor, radius: 16, x: 0, y: 6)
+                    RegionCardView(label: "The Americas", description: "Latin & north america", centerText: "Coming Soon", backgroundColor: OstelcoColor.azul.toColor)
                     
                     ForEach(countries, id: \.countryCode ) { country in
                         OstelcoContainer {
@@ -119,6 +74,8 @@ struct CoverageView: View {
                     }
                 }.padding()
             }
+        }.sheet(isPresented: $showModal) {
+            RegionView()
         }
     }
 }
