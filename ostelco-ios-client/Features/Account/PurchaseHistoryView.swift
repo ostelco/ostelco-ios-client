7b2bdb30461f8dec0d01aa5c6cfc35890f71a348
Håvard Norén <havardnoren@gmail.com>
2019-10-22 00:22:01 +0200

    Style PurchaseHistoryView according to design.

--- ostelco-ios-client/Features/Account/PurchaseHistoryView.swift
+++ ostelco-ios-client/Features/Account/PurchaseHistoryView.swift
@@ -16,12 +16,18 @@ struct PurchaseHistoryView: View {
     var body: some View {
         VStack {
             List(store.purchaseRecords, id: \.name) { record in
-                VStack(alignment: .leading) {
+                VStack(alignment: .leading, spacing: 10) {
                     Text(record.date)
+                        .font(.system(size: 17, weight: .bold))
+                        .foregroundColor(OstelcoColor.inputLabelAny.toColor)
                     HStack {
                         Text(record.name)
+                            .font(.system(size: 17))
+                            .foregroundColor(OstelcoColor.inputLabelAny.toColor)
                         Spacer()
                         Text(record.amount)
+                            .font(.system(size: 17))
+                            .foregroundColor(OstelcoColor.inputLabelAny.toColor)
                     }
                 }.padding()
             }
