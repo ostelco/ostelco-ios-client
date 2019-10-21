6399ed6b33b58ecf995c1f4bccd0effa61cff744
Håvard Norén <havardnoren@gmail.com>
2019-10-21 23:52:30 +0200

    Change OYA not available in message.

--- ostelco-ios-client/Features/Coverage/RegionListByLocation.swift
+++ ostelco-ios-client/Features/Coverage/RegionListByLocation.swift
@@ -52,7 +52,7 @@ struct RegionListByLocation: View {
                 )
             } else {
                 return AnyView(
-                    Text("Unfortunately OYA is currently not available in\n\(country.nameOrPlaceholder)")
+                    Text("Unfortunately, OYA is currently not\navailable in \(country.nameOrPlaceholder) :(")
                         .multilineTextAlignment(.center)
                         .lineLimit(nil)
                 )
